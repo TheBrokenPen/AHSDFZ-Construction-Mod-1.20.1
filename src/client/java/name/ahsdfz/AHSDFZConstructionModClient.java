afef74a2ac597f7e3772c8d850a1d37631a74e53
ExpertPbSeal <pobi1234@163.com>
2024-09-26 23:15:11 +0800

    Initial Commit Second Try

---
+++ src/client/java/name/ahsdfz/AHSDFZConstructionModClient.java
@@ -0,0 +1,10 @@
+package name.ahsdfz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AHSDFZConstructionModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/name/ahsdfz/AHSDFZConstructionModDataGenerator.java
@@ -0,0 +1,11 @@
+package name.ahsdfz;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AHSDFZConstructionModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
