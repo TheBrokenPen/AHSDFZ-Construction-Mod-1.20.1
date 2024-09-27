63ad5f299457acc0eb5cfb025cbe77313ef58eda
TheBrokenPen <thebrokenpen@sina.com>
2024-09-27 09:06:23 +0800

    Fixed

--- src/main/java/name/ahsdfz/registry/ModItems.java
+++ src/main/java/name/ahsdfz/registry/ModItems.java
@@ -6,13 +6,21 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
+//#
+//#
+//#
+//#
+
 public class ModItems {
-    public static Item registerItem(String name, Item item, ItemGroup...itemGroups) {
+    public static Item registerItem(String name, Item item, RegistryKey<ItemGroup>...itemGroups) {
         Item registeredItem = Registry.register(Registries.ITEM, new Identifier(AHSDFZConstructionMod.MOD_ID, name), item);
-        for (ItemGroup itemGroup: itemGroups) {
-            ItemGroupEvents.modifyEntriesEvent()
+        for (RegistryKey<ItemGroup> itemGroup: itemGroups) {
+            ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(registeredItem));
         }
+        return registeredItem;
     }
 }
+
