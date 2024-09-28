053450724a320160507972e96c5b977e5e4740dc
TheBrokenPen <thebrokenpen@sina.com>
2024-09-28 16:18:54 +0800

    N/A

--- src/main/java/name/ahsdfz/registry/ModItemGroup.java
+++ src/main/java/name/ahsdfz/registry/ModItemGroup.java
@@ -14,13 +14,13 @@ public class ModItemGroup {
     public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY_1 = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(AHSDFZConstructionMod.MOD_ID, "ahsdfz_item_group_1"));
     public static final ItemGroup ItemGroup1 = FabricItemGroup.builder()
             .displayName(Text.translatable("itemgroup.ahsdfz-construction-mod.BuildingBlocks"))
-            .icon(() -> new ItemStack(ModItems.TestItem1))
+            .icon(() -> new ItemStack(ModItems.TestificantItem))
             .build();
 
     public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY_2 = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(AHSDFZConstructionMod.MOD_ID, "ahsdfz_item_group_2"));
     public static final ItemGroup ItemGroup2 = FabricItemGroup.builder()
             .displayName(Text.translatable("itemgroup.ahsdfz-construction-mod.Items"))
-            .icon(() -> new ItemStack(ModItems.TestItem2))
+            .icon(() -> new ItemStack(ModItems.NormalItem))
             .build();
 
 //    public static final TestGroup = FabricItemGroup.builder()
--- src/main/java/name/ahsdfz/registry/ModItems.java
+++ src/main/java/name/ahsdfz/registry/ModItems.java
@@ -1,51 +1,38 @@
 package name.ahsdfz.registry;
 
-//FunctionImports
 import name.ahsdfz.AHSDFZConstructionMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    //Main Item Register
-    public static final Item TestItem1 = registerItem("test1",
-            new Item(new FabricItemSettings()),ModItemGroup.ItemGroup1,);
-    public static final Item TestItem2 = registerItem("test2",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem3 = registerItem("test3",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem4 = registerItem("test4",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem5 = registerItem("test5",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem6 = registerItem("test6",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem7 = registerItem("test7",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem8 = registerItem("test8",
-            new Item(new FabricItemSettings()));
-    public static final Item TestItem9 = registerItem("test9",
-            new Item(new FabricItemSettings()));
+    //Registered Items
+    public static final Item TestificantItem = registerItems("testificant_item", new Item(new FabricItemSettings()));
+    public static final Item NormalItem = registerItems("normal_item", new Item(new FabricItemSettings()));
 
-    //ItemRegistryGadget
-    @SafeVarargs
-    public static Item registerItem(String name, Item item, RegistryKey<ItemGroup>...itemGroups) {
-        Item registeredItem = Registry.register(Registries.ITEM, new Identifier(AHSDFZConstructionMod.MOD_ID, name), item);
-        for (RegistryKey<ItemGroup> itemGroup: itemGroups) {
-            ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(registeredItem));
-        }
-        return registeredItem;
+    //ItemGroups
+    public static void addTestificantItem(FabricItemGroupEntries fabricItemGroupEntries) {
+        fabricItemGroupEntries.add(TestificantItem);
     }
+    public static void addNormalItem(FabricItemGroupEntries fabricItemGroupEntries) {
+        fabricItemGroupEntries.add(NormalItem);
+    }
+
+    //Registry Gadget
+    private static Item registerItems(String name, Item item) {
+        return Registry.register(Registries.ITEM,new Identifier(AHSDFZConstructionMod.MOD_ID, name), item);
+    }
+
+    //Register Items to Groups
     public static void registerModItems() {
-        AHSDFZConstructionMod.LOGGER.debug("Registering mod items for" + AHSDFZConstructionMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.OPERATOR).register(ModItems::addTestificantItem);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addNormalItem);
     }
 }
-
-//WrongExample: public static final Item TestItem = registerItem(name;"test", new item(new FabricItemSettings()));
